95d2b44210eade0df03bf647c68a23c451941807
AgungDH <agungdh@live.com>
2019-06-26 15:57:01 +0700

    .

--- application/controllers/Temp.php
+++ application/controllers/Temp.php
@@ -5,10 +5,57 @@ use Illuminate\Database\Capsule\Manager as DB;
 
 use application\eloquents\Kendaraan as Kendaraan_model;
 use application\eloquents\Transaksi as Transaksi_model;
+use application\eloquents\PemilikKendaraan as PemilikKendaraan_model;
 
 class Temp extends CI_Controller {
 	public function index()
 	{
-		echo helper()->textKePemilik(2);
+		dd(
+			[
+				self::textBelumBayarPerPemilikKendaraan(),
+				self::dataBelumBayar(),
+			]
+		);
+	}
+
+	public function textBelumBayarPerPemilikKendaraan()
+	{
+		$belumBayarsPerPemilik = [];
+		foreach (PemilikKendaraan_model::all() as $item) {
+			$belumBayarsPerPemilik[$item->id] = helper()->jumlahBelumBayar($item->id);
+
+			if ($belumBayarsPerPemilik[$item->id]['jumlah'] == 0) {
+				unset($belumBayarsPerPemilik[$item->id]);
+			}			
+		}
+
+		$datas = [];
+		foreach ($belumBayarsPerPemilik as $key => $value) {
+			$datas[$key] = helper()->textKePemilik($key);	
+		}
+
+		return compact(['datas']);
+	}
+
+	public function dataBelumBayar()
+	{
+		$belumBayarsPerPemilik = [];
+		foreach (PemilikKendaraan_model::all() as $item) {
+			$belumBayarsPerPemilik[$item->id] = helper()->jumlahBelumBayar($item->id);
+
+			if ($belumBayarsPerPemilik[$item->id]['jumlah'] == 0) {
+				unset($belumBayarsPerPemilik[$item->id]);
+			}
+		}
+
+		$belumBayars = [];
+		$belumBayars['total'] = 0;
+		$belumBayars['jumlah'] = 0;
+		foreach ($belumBayarsPerPemilik as $item) {
+			$belumBayars['total'] += $item['total'];
+			$belumBayars['jumlah'] += $item['jumlah'];
+		}
+
+		return compact(['belumBayarsPerPemilik', 'belumBayars']);
 	}
 }
